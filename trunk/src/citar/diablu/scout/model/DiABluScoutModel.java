37381683f1a9655f08c6d5553ff9b6c602e8b244
nunoalexandre.rodrigues <nunoalexandre.rodrigues@a013ffd2-dc1c-0410-a766-57759f07c308>
2006-09-08 15:42:47 +0000

    First packages & source files. Still missing sources, i will add them as soon as created

---
+++ trunk/src/citar/diablu/scout/model/DiABluScoutModel.java
@@ -0,0 +1,22 @@
+/*
+ * DiABluScoutModel.java
+ *
+ * Created on 5 de Setembro de 2006, 23:18
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package citar.diablu.scout.model;
+
+/**
+ *
+ * @author Nuno Rodrigues
+ */
+public class DiABluScoutModel {
+    
+    /** Creates a new instance of DiABluScoutModel */
+    public DiABluScoutModel() {
+    }
+    
+}
