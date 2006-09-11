81cbbd5a14c586e70d8fd3d88cceed8e865ed5c4
nunoalexandre.rodrigues <nunoalexandre.rodrigues@a013ffd2-dc1c-0410-a766-57759f07c308>
2006-09-11 17:07:51 +0000

    Updated source files to include the GPL 2 sentence at the beginning

--- trunk/diablu server beta/src/citar/diablu/com/osc/DiABluOSC.java
+++ trunk/diablu server beta/src/citar/diablu/com/osc/DiABluOSC.java
@@ -1,10 +1,22 @@
+
 /*
  * DiABluOSC.java
  *
  * Created on 11 de Maio de 2006, 16:11
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; 
+ * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ * MA 02111-1307 USA
+ *
  */
 
 package citar.diablu.com.osc;
@@ -32,7 +44,7 @@ public class DiABluOSC {
     /**
      * OSC Commands used
      */
-    String DEVICEIN="/DeviceIn";                    // New Device entered 
+    String DEVICEIN="/DeviceIn";                    // New Device entered
     String DEVICESIN="/DeviceListIn";               // Newly entered devices //ss ss ss
     String DEVICEOUT="/DeviceOut";                  // Single Device exited
     String DEVICESOUT="/DeviceListOut";             // Multiple Devices out //ss ss ss
@@ -48,94 +60,94 @@ public class DiABluOSC {
     }
     
     
-    /** 
+    /**
      * This method outputs the added devices present in the Vector
      * to 2 OSC commands /devicein & /devicesin
      * /devicein sends an OSC Bundle with all the devices
      * /devicesin sends a Single OSC Message with all the devices concatenated in
      *
-     */    
+     */
     public void sendAddDevices(Vector aDevices, InetSocketAddress addr){
-       
-       int totalDevicesAdded = 0;  // size of the list
-       String[][] tempAddDevices1 = null;
-       Object[] tempAddDevices2 = null;
-       
-       // paranoid check
-       if (aDevices == null || aDevices.size() == 0){
-           System.out.println("[DiABluOSC-sendAddDevices()]Error!Received an empty list!!");
-           return;
-       }
-       
-       // get the vector's size'
-       totalDevicesAdded = aDevices.size();
-       
-       tempAddDevices1 = new String[totalDevicesAdded][totalDevicesAdded]; // devicein
-       tempAddDevices2 = new Object[totalDevicesAdded];                    // devicesin
-       
-       // Convert the info
-       tempAddDevices1 = vectorTOstring(aDevices);
-       tempAddDevices2 = vectorTOobject(aDevices);
-       
-       // Send it...
-       
-       // OSC /devicein
-       sendBundle("/devicein",tempAddDevices1,addr);
-       
-       // OSC /devicesin
-       sendMessage("/devicesin",tempAddDevices2,addr);
-       
+        
+        int totalDevicesAdded = 0;  // size of the list
+        String[][] tempAddDevices1 = null;
+        Object[] tempAddDevices2 = null;
+        
+        // paranoid check
+        if (aDevices == null || aDevices.size() == 0){
+            System.out.println("[DiABluOSC-sendAddDevices()]Error!Received an empty list!!");
+            return;
+        }
+        
+        // get the vector's size'
+        totalDevicesAdded = aDevices.size();
+        
+        tempAddDevices1 = new String[totalDevicesAdded][totalDevicesAdded]; // devicein
+        tempAddDevices2 = new Object[totalDevicesAdded];                    // devicesin
+        
+        // Convert the info
+        tempAddDevices1 = vectorTOstring(aDevices);
+        tempAddDevices2 = vectorTOobject(aDevices);
+        
+        // Send it...
+        
+        // OSC /devicein
+        sendBundle("/devicein",tempAddDevices1,addr);
+        
+        // OSC /devicesin
+        sendMessage("/devicesin",tempAddDevices2,addr);
+        
     }
     
-    /** 
+    /**
      * This method outputs the removed devices present in the Vector
      * to 2 OSC commands /deviceout & /devicesout
      * /deviceout sends an OSC Bundle with all the devices
      * /devicesout sends a Single OSC Message with all the devices concatenated in
      *
-     */ 
+     */
     public void sendRemoveDevices(Vector rDevices, InetSocketAddress addr){
-              
-       int totalDevicesRemoved = rDevices.size();
-       String[][] tempRemoveDevices1 = new String[totalDevicesRemoved][totalDevicesRemoved];
-       Object[] tempRemoveDevices2 = new Object[totalDevicesRemoved];
-       
-       // Convert the info
-       tempRemoveDevices1 = vectorTOstring(rDevices);
-       tempRemoveDevices2 = vectorTOobject(rDevices);
-       
-       // Send it...
-       
-       // OSC /deviceout
-       sendBundle(DEVICEOUT,tempRemoveDevices1,addr);
-       
-       // OSC /devicesout
-       sendMessage(DEVICESOUT,tempRemoveDevices2,addr);
-       
+        
+        int totalDevicesRemoved = rDevices.size();
+        String[][] tempRemoveDevices1 = new String[totalDevicesRemoved][totalDevicesRemoved];
+        Object[] tempRemoveDevices2 = new Object[totalDevicesRemoved];
+        
+        // Convert the info
+        tempRemoveDevices1 = vectorTOstring(rDevices);
+        tempRemoveDevices2 = vectorTOobject(rDevices);
+        
+        // Send it...
+        
+        // OSC /deviceout
+        sendBundle(DEVICEOUT,tempRemoveDevices1,addr);
+        
+        // OSC /devicesout
+        sendMessage(DEVICESOUT,tempRemoveDevices2,addr);
+        
     }
     
-     /** 
+    /**
      * This method sends the list of all devices detected by the DB System
      * to the OSC commands /devicelist
      *
-     */ 
+     */
     public void sendDeviceList(Vector lDevices, InetSocketAddress addr){
         
         // first verify that we've got some info
         if (lDevices != null) {
-         
+            
             int totalDevicesFound = lDevices.size();
             //Object[] tempListDevices = new Object();
-        
+            
             // Convert the info
             Object[] tempListDevices = vectorTOobject(lDevices);
-        
+            
             // OSC /devicelist
             sendMessage(DEVICELIST,tempListDevices,addr);
         } else return;
         
     }
-        
+    
     public void sendMsg(DiABluMsg newDMsg, InetSocketAddress addr){
         
         // get the sender's related ID
@@ -167,7 +179,7 @@ public class DiABluOSC {
         Object [] oscMsg = new Object[] { uuidT, fnameT, keypressedT, gactionT };
         
         // send the keys
-        sendMessage(KEYIN,oscMsg,addr);        
+        sendMessage(KEYIN,oscMsg,addr);
         
     }
     
@@ -186,7 +198,7 @@ public class DiABluOSC {
         
         DiABluDevice dbT = null;
         String uuidT = "";
-        String fnameT = "";        
+        String fnameT = "";
         Object[] oscMsg = null;
         
         for (int i=0;i<totalNamesChanged;i++){
@@ -202,7 +214,7 @@ public class DiABluOSC {
             // send it...
             sendMessage(NAMECHANGED,oscMsg,addr);
         }
-                              
+        
     }
     
     /**
@@ -210,9 +222,9 @@ public class DiABluOSC {
      */
     public void sendDeviceCount(int dCount, InetSocketAddress addr){
         
-        sendMessage(DEVICECOUNT,dCount,addr);        
+        sendMessage(DEVICECOUNT,dCount,addr);
         
-    }               
+    }
     
     /**
      *  This method converts a vector of DiABlu Devices into a String[][] array
@@ -225,7 +237,7 @@ public class DiABluOSC {
         String uuidT = "";
         String fnameT = "";
         DiABluDevice ddT;
-        int tempSize = xDevices.size();        
+        int tempSize = xDevices.size();
         String[][] tempDBstring = new String[tempSize][2];
         
         for (int i=0; i<tempSize; i++){
@@ -244,7 +256,7 @@ public class DiABluOSC {
         }
         
         return tempDBstring;
-    } 
+    }
     
     /**
      *  This method converts a vector of DiABlu Devices into a Object[] array
@@ -262,11 +274,11 @@ public class DiABluOSC {
         Object[] tempDBobject = null;      // returned array
         
         if ( xDevices == null || xDevices.size() == 0 ) {
-                    
+            
             System.out.println("[DiABluOSC-vectorTOobject()]ERROR!!vectorTOobject Method has received an empty list!!");
             return null;
             
-        }  
+        }
         
         // get the size
         tempSize = xDevices.size();
@@ -275,8 +287,8 @@ public class DiABluOSC {
         tempDBobject = new Object[2*tempSize];
         
         // debug info
-        System.out.println("[DiABluOSC-vectorTOobject()]List size:"+xDevices.size());        
-                
+        System.out.println("[DiABluOSC-vectorTOobject()]List size:"+xDevices.size());
+        
         for (int i=0; i<tempSize; i++){
             
             // get the device
@@ -292,24 +304,24 @@ public class DiABluOSC {
             tempCounter+=2;
             
         }
-       
-         // debug info
-        System.out.println("[DiABluOSC-vectorTOobject()]Returned list size:"+tempDBobject.length);  
+        
+        // debug info
+        System.out.println("[DiABluOSC-vectorTOobject()]Returned list size:"+tempDBobject.length);
         
         return tempDBobject;
-       
         
-       }
-                
+        
+    }
+    
     
     /**
      * This method sends a Bundle of several DiABlu Devices
-     * Each device is sended on the form 
+     * Each device is sended on the form
      * [UUID][FName][Key Pressed(if any)][gAction(if any)][text(if any)]
      *
-     */ 
+     */
     
-    public void sendBundle ( String oscCommand, String[][] brutus , InetSocketAddress addr ){
+    public void sendBundle( String oscCommand, String[][] brutus , InetSocketAddress addr ){
         
         final long serverLatency = 50;
         final long now = System.currentTimeMillis() + serverLatency;
@@ -318,152 +330,148 @@ public class DiABluOSC {
         OSCBundle bundle;
         OSCTransmitter trns;
         
-               
-	try {
-	        dch = DatagramChannel.open();
-                dch.configureBlocking(true);
-                trns = new OSCTransmitter(dch, addr);
-                bundle = new OSCBundle (now);
+        
+        try {
+            dch = DatagramChannel.open();
+            dch.configureBlocking(true);
+            trns = new OSCTransmitter(dch, addr);
+            bundle = new OSCBundle(now);
+            
+            for ( int i = 0; i < brutus.length ; i++ ){
                 
-                for ( int i = 0; i < brutus.length ; i++ ){
-                    
-                    System.out.println("[DiABluOSC-sendBundle()]Processing packets...["+brutus[i][0]+"]["+brutus[i][1]+"]\n");
-                    Object[] objT = new Object[] { brutus[i][0] , brutus[i][1] };
-                    bundle.addPacket( new OSCMessage (oscCommand, objT));
+                System.out.println("[DiABluOSC-sendBundle()]Processing packets...["+brutus[i][0]+"]["+brutus[i][1]+"]\n");
+                Object[] objT = new Object[] { brutus[i][0] , brutus[i][1] };
+                bundle.addPacket( new OSCMessage(oscCommand, objT));
                 
-                }
-                
-                /* Sending message */
-                System.out.print("Sending [OSC Bundle:" + oscCommand + "] | [BRUTUS]\n");
-		trns.send(bundle, addr);
-	}
-
-	catch( Exception e1 ) {
+            }
+            
+            /* Sending message */
+            System.out.print("Sending [OSC Bundle:" + oscCommand + "] | [BRUTUS]\n");
+            trns.send(bundle, addr);
+        }
+        
+        catch( Exception e1 ) {
             // TODO: catch this exception
             // printException( e1 );
-	}
-	finally {
+        } finally {
             if( dch != null ) {
-              try {
-                  dch.close();
-              } catch( Exception e2 ) {
-                // TODO: catch this exception
-                // printException( e2 );
-              };
+                try {
+                    dch.close();
+                } catch( Exception e2 ) {
+                    // TODO: catch this exception
+                    // printException( e2 );
+                };
             }
-        }  
-     }
+        }
+    }
     
     /**
      * Sends a simple osc message
      */
-    public static void sendMessage ( String oscCommand, String message, InetSocketAddress addr ){
+    public static void sendMessage( String oscCommand, String message, InetSocketAddress addr ){
         
         DatagramChannel dch = null;
         OSCTransmitter trns;
-               
-	try {
-	        dch = DatagramChannel.open();
-                trns = new OSCTransmitter(dch, addr);                
-                
-                /* Sending message */
-                System.out.print("Sending Message...OSC:" + oscCommand + " | Content:" + message + "\n");
-		trns.send( new OSCMessage( oscCommand, new Object[] { 
-                                                                   message
-                                                               }
-                                          ),addr);
-	}
-
-	catch( Exception e1 ) {
+        
+        try {
+            dch = DatagramChannel.open();
+            trns = new OSCTransmitter(dch, addr);
             
-                // TODO: catch this exception
-                // printException( e1 );
-	}
-	finally {
-            if( dch != null ) {
-              try {
-                  dch.close();
-              } catch( Exception e2 ) {
-                  
-                  // TODO: catch this exception
-                  //printException( e2 );
-              };
+            /* Sending message */
+            System.out.print("Sending Message...OSC:" + oscCommand + " | Content:" + message + "\n");
+            trns.send( new OSCMessage( oscCommand, new Object[] {
+                message
             }
-        }   
+            ),addr);
+        }
+        
+        catch( Exception e1 ) {
+            
+            // TODO: catch this exception
+            // printException( e1 );
+        } finally {
+            if( dch != null ) {
+                try {
+                    dch.close();
+                } catch( Exception e2 ) {
+                    
+                    // TODO: catch this exception
+                    //printException( e2 );
+                };
+            }
+        }
         
     }
     
     /**
      * Sends a composite Object[] message
      */
-    public static void sendMessage ( String oscCommand, Object[] brutus , InetSocketAddress addr ){
+    public static void sendMessage( String oscCommand, Object[] brutus , InetSocketAddress addr ){
         
         
         DatagramChannel dch = null;
         OSCTransmitter trns;
-               
-	try {
-	        dch = DatagramChannel.open();
-                trns = new OSCTransmitter(dch, addr);
-                
-                
-                /* Sending message */
-                System.out.print("Sending [OSC Command:" + oscCommand + "] | [List Size:"+brutus.length+"]\n");
-		trns.send( new OSCMessage( oscCommand, brutus
-                                          ),addr);
-	}
-
-	catch( Exception e1 ) {
+        
+        try {
+            dch = DatagramChannel.open();
+            trns = new OSCTransmitter(dch, addr);
+            
+            
+            /* Sending message */
+            System.out.print("Sending [OSC Command:" + oscCommand + "] | [List Size:"+brutus.length+"]\n");
+            trns.send( new OSCMessage( oscCommand, brutus
+                    ),addr);
+        }
+        
+        catch( Exception e1 ) {
             //printException( e1 );
-	}
-	finally {
+        } finally {
             if( dch != null ) {
-              try {
-                  dch.close();
-              } catch( Exception e2 ) {
-                  
-                 //printException( e2 );
-              };
+                try {
+                    dch.close();
+                } catch( Exception e2 ) {
+                    
+                    //printException( e2 );
+                };
             }
-        }  
-     }
-     
+        }
+    }
+    
     /**
      * Sends simple OSC message with a [int]
      * Suited for /devicecount
-     */ 
-    public static void sendMessage ( String oscCommand, int num , InetSocketAddress addr ){
+     */
+    public static void sendMessage( String oscCommand, int num , InetSocketAddress addr ){
         
         DatagramChannel dch = null;
         OSCTransmitter trns;
-               
-	try {
-	        dch = DatagramChannel.open();
-                trns = new OSCTransmitter(dch, addr);
-                
-                
-                /* Sending message */
-                System.out.print("Sending [OSC Command:" + oscCommand + "] | [Count:"+num+"]\n");
-		trns.send( new OSCMessage( oscCommand, new Object[] { 
-                                                                        new Integer(num) 
-                                                                    }
-                                          ),addr);
-	}
-
-	catch( Exception e1 ) {
-           //TODO:Handle this exception     
-           //printException( e1 );
-	}
-	finally {
-            if( dch != null ) {
-              try {
-                  dch.close();
-              } catch( Exception e2 ) {
-                  //TODO:Handle this exception
-                  //printException( e2 );
-              };
+        
+        try {
+            dch = DatagramChannel.open();
+            trns = new OSCTransmitter(dch, addr);
+            
+            
+            /* Sending message */
+            System.out.print("Sending [OSC Command:" + oscCommand + "] | [Count:"+num+"]\n");
+            trns.send( new OSCMessage( oscCommand, new Object[] {
+                new Integer(num)
             }
-        }  
+            ),addr);
+        }
+        
+        catch( Exception e1 ) {
+            //TODO:Handle this exception
+            //printException( e1 );
+        } finally {
+            if( dch != null ) {
+                try {
+                    dch.close();
+                } catch( Exception e2 ) {
+                    //TODO:Handle this exception
+                    //printException( e2 );
+                };
+            }
+        }
     }
     
 }
--- trunk/diablu server beta/src/citar/diablu/gui/DiABluCR.java
+++ trunk/diablu server beta/src/citar/diablu/gui/DiABluCR.java
@@ -2,6 +2,18 @@
  * DiABluCR.java
  *
  * Created on 18 de Abril de 2006, 11:00
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; 
+ * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ * MA 02111-1307 USA
  */
 
 package citar.diablu.gui;
@@ -40,7 +52,7 @@ public class DiABluCR extends javax.swing.JDialog {
         ds_jl.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         ds_jl.setText("DiABlu Server");
 
-        version_jl.setText("Alpha Version 0.22");
+        version_jl.setText("Version 1.0");
 
         development_jl.setText("Development: Jorge Cardoso, Nuno Rodrigues");
 
