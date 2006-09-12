05ce1d890d280fe070a54f10a248bfe05c8c82e3
nunoalexandre.rodrigues <nunoalexandre.rodrigues@a013ffd2-dc1c-0410-a766-57759f07c308>
2006-09-12 17:16:51 +0000

    updated source to include the gpl 2 license

--- diablu mobile beta/src/com/bt/dbBTdiscovery.java
+++ diablu mobile beta/src/com/bt/dbBTdiscovery.java
@@ -3,8 +3,22 @@
  *
  * Created on 20 juillet 2006, 13:58
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ * Copyright (C) 2006 CITAR
+ * This is part of the DiABlu Project
+ * http://diablu.jorgecardoso.org
+ * Created by Jorge Cardoso(jccardoso@porto.ucp.pt) & Nuno Rodrigues(nunoalexandre.rodrigues@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; 
+ * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ * MA 02111-1307 USA
  */
 
 package com.bt;
