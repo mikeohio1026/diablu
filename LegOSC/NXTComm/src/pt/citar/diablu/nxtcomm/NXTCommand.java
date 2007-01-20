fc06d33e398a79fa905856da301322ac7d31f75e
jorgediablu <jorgediablu@a013ffd2-dc1c-0410-a766-57759f07c308>
2007-01-20 13:32:59 +0000

    Added code to format the packet buffer to hex output. Added JavaDoc documentation.

--- LegOSC/NXTComm/src/pt/citar/diablu/nxtcomm/NXTCommand.java
+++ LegOSC/NXTComm/src/pt/citar/diablu/nxtcomm/NXTCommand.java
@@ -9,12 +9,16 @@ package pt.citar.diablu.nxtcomm;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.IOException;
+import java.util.Formatter;
 
 
 /**
  * Represents a Command that can be sent to the NXTBrick. This class cannot be instantiated. 
  * Only subclasses which represent concrete Commands should be used.
+ * Commands can generate Responses if so required. Responses are encapsulated by  <code>NXTResponse</code> subclasses.
  *
+ * Subclasses should implement the <code>sendCommand()</code> method and any specific parameter setting methods.
+ * @see NXTResponse
  * @author Jorge Cardoso
  */
 public abstract class NXTCommand {
@@ -44,13 +48,21 @@ public abstract class NXTCommand {
      */
     protected byte []buffer;
     
-    /** Creates a new instance of NXTCommand */
+    /**
+     * Constructs a new command object with no response requirement.
+     */
     public NXTCommand() {
         
         /* By default we should not require a response since it's faster this way*/
         this(false);
     }
     
+    /**
+     * Constructs a new command object with a response requirement.
+     *
+     * @param responseRequired The response requirement. <code>false</code>, no response required. <code>true</code>, response
+     * required.
+     */
     public NXTCommand(boolean responseRequired) {
             
         this.responseRequired = responseRequired;
@@ -62,16 +74,38 @@ public abstract class NXTCommand {
     public abstract NXTResponse sendCommand(InputStream is, OutputStream os) throws IOException;
 
     
+    /**
+     * Returns the response requirements for the command.
+     *
+     * @return The response requirement.
+     */
     public boolean isResponseRequired() {
         return responseRequired;
     }
 
+    /**
+     * Sets the requirement for a response to the command.
+     * If a response to a command is not strictly necessary that this should be set to false in
+     * order to achieve a faster communication.
+     *
+     * @param responseRequired <code>true</code> if a response is required, <code>false</code> if a response is not required.
+     */
     public void setResponseRequired(boolean responseRequired) {
         this.responseRequired = responseRequired;
     }
     
+    /**
+     * Formats the command packet in hex form.
+     *
+     * @return The command packet in hex form.
+     */
     public String toString() {
-        return null;
+        Formatter f = new Formatter();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < buffer.length; i++) {
+            f.format("%x ", buffer[i]);
+        }
+        return f.toString();
         
     }
     
