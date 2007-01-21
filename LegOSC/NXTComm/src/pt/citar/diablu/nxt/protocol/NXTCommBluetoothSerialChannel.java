78b5dd81c7998b28c57ccef125da335d28ded018
jorgediablu <jorgediablu@a013ffd2-dc1c-0410-a766-57759f07c308>
2007-01-21 13:50:37 +0000

    Added GPL license message at the start of the source code.

--- LegOSC/NXTComm/src/pt/citar/diablu/nxt/protocol/NXTCommBluetoothSerialChannel.java
+++ LegOSC/NXTComm/src/pt/citar/diablu/nxt/protocol/NXTCommBluetoothSerialChannel.java
@@ -3,8 +3,26 @@
  *
  * Created on 21 de Janeiro de 2007, 12:48
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ *  LegOSC: and OSC gateway to control the Lego Minstorms NXT robots.
+ *  This is part a of the DiABlu Project (http://diablu.jorgecardoso.org)
+ *
+ *  Copyright (C) 2007  Jorge Cardoso
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  You can reach me by email: jorgecardoso <> ieee org
  */
 
 package pt.citar.diablu.nxt.protocol;
