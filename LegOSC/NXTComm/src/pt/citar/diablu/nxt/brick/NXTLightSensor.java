e016b56b2b84882bcaf3749c3b4848974d39783b
jorgediablu <jorgediablu@a013ffd2-dc1c-0410-a766-57759f07c308>
2007-07-17 14:22:06 +0000

    Implemented Light Sensor

--- LegOSC/NXTComm/src/pt/citar/diablu/nxt/brick/NXTLightSensor.java
+++ LegOSC/NXTComm/src/pt/citar/diablu/nxt/brick/NXTLightSensor.java
@@ -29,14 +29,77 @@
 
 package pt.citar.diablu.nxt.brick;
 
+import java.io.IOException;
+import pt.citar.diablu.nxt.protocol.*;
+
 /**
+ * The NXTLightSensor class represents the, hmm, light sensor...
  *
  * @author Jorge Cardoso
  */
 public class NXTLightSensor extends NXTComponent {
     
-    public NXTLightSensor() {
+    public static final byte ACTIVE = NXTResponseInputValues.LIGHT_ACTIVE_TYPE;
+    public static final byte INACTIVE = NXTResponseInputValues.LIGHT_INACTIVE_TYPE;
+    
+    private NXTCommandGetInputValues getInputValues;
+    private NXTResponseInputValues inputValues;
+    
+    /**
+     * The sensor type. ACTIVE or INACTIVE.
+     */
+    private byte type;
+    
+    /**
+     * Coonstructs a new NXTLightSensor object given the NXTBrick and the port number the
+     * sensor is connected to.
+     *
+     * @param brick The NXTBrick.
+     * @param portAttached The port to which the sensor is attached. (0 - 3).
+     */
+    public NXTLightSensor(NXTBrick brick, byte portAttached) {
+        super(brick, portAttached);
+        initialize();
+    }
+    
+    public void initialize() {
+        this.type = INACTIVE;
+        NXTCommandSetInputMode inputMode = new NXTCommandSetInputMode(this.portAttached,
+                this.type,
+                NXTResponseInputValues.PCT_FULL_SCALE_MODE);
+        try {
+            this.brick.getChannel().sendCommand(inputMode);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        getInputValues = new NXTCommandGetInputValues(this.portAttached);
+    }
+    
+    public int getValue() {
+        try {
+            inputValues = (NXTResponseInputValues)brick.getChannel().sendCommand(getInputValues);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return -1;
+        }
+        return inputValues.getScaledValue();
+    }
+    
+    public void setType(int type) throws IllegalArgumentException {
+        
+        if (type != ACTIVE && type != INACTIVE) {
+            throw new IllegalArgumentException("Wrong argument: " + type + ". Should be ACTIVE or INACTIVE.");
+        }
+        this.type = (byte)type;
+        
+        NXTCommandSetInputMode inputMode = new NXTCommandSetInputMode(this.portAttached,
+                this.type,
+                NXTResponseInputValues.PCT_FULL_SCALE_MODE);
+        try {
+            this.brick.getChannel().sendCommand(inputMode);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
         
     }
-
 }
