380173f3b0a9d649300d61026d4e95bbfa417834
jorgediablu <jorgediablu@a013ffd2-dc1c-0410-a766-57759f07c308>
2007-07-19 15:31:38 +0000

    Added license message.

--- trunk/LegOSC/LegOSC/src/pt/citar/diablu/legosc/LegOSC.java
+++ trunk/LegOSC/LegOSC/src/pt/citar/diablu/legosc/LegOSC.java
@@ -1,10 +1,30 @@
 /*
- * Main.java
+ * LegOSC.java
  *
  * Created on 23 de Janeiro de 2007, 0:13
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ *  LegOSC: An OSC gateway to control the NXT Brick.
+ *  This is part a of the DiABlu Project (http://diablu.jorgecardoso.org)
+ *
+ *  Copyright (C) 2007  Jorge Cardoso
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  You can reach me by:
+ *  email: jorgecardoso <> ieee org
+ *  web: http://jorgecardoso.org
  */
 package pt.citar.diablu.legosc;
 
