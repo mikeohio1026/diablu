dd26b292e25b9d8cb1de7f45f21be56db5f7ec25
pedro.r.santos <pedro.r.santos@a013ffd2-dc1c-0410-a766-57759f07c308>
2008-11-05 17:24:30 +0000

    Logger implemented

--- trunk/SMS2OSC/src/pt/citar/diablu/sms2osc/S2O.java
+++ trunk/SMS2OSC/src/pt/citar/diablu/sms2osc/S2O.java
@@ -1,14 +1,19 @@
 package pt.citar.diablu.sms2osc;
 
+import java.io.IOException;
 import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import pt.citar.diablu.sms2osc.bluetooth.S2OBTConnection;
 import pt.citar.diablu.sms2osc.gui.S2OGUI;
 import pt.citar.diablu.sms2osc.osc.S2OOscClient;
 import pt.citar.diablu.sms2osc.osc.S2OOscServer;
 import pt.citar.diablu.sms2osc.util.S2OCommPortList;
 import pt.citar.diablu.sms2osc.util.S2OProperties;
+import pt.citar.diablu.sms2osc.util.S2OTextAreaHandler;
 
 
 public class S2O {
@@ -16,10 +21,13 @@ public class S2O {
     private Logger logger;
     private S2OCommPortList commPortList;
     private S2OProperties properties;
-    private ConsoleHandler ch = new ConsoleHandler();
+    
     
     
     private S2OGUI gui;
+    private S2OTextAreaHandler textAreaHandler;
+    private ConsoleHandler consoleHandler;
+    private FileHandler fileHandler;
     private S2OOscServer oscServer;
     private S2OOscClient oscClient;
     
@@ -30,26 +38,55 @@ public class S2O {
     public S2O()
     {
         logger = Logger.getLogger("pt.citar.diablu.sms2osc");
-        ch.setLevel(Level.ALL);
-        logger.addHandler(ch);
-        System.out.println(logger.getHandlers()[0].getLevel());
-        
-        logger.log(Level.SEVERE, "start");
-        commPortList = new S2OCommPortList();
         properties = new S2OProperties(this);
+        commPortList = new S2OCommPortList();
+        gui = new S2OGUI(this);
+        setupLogger();
+        
         properties.loadProperties();
         oscServer = new S2OOscServer(this);
         oscClient = new S2OOscClient(this);
-        gui = new S2OGUI(this);
+        
         btConnection = new S2OBTConnection(this, properties.getGateway(), "COM6", 57600, "Siemens", "S65");
+        
+        
     
         
     }
+    
     
     public static void main(String[] args) {
     
         S2O s2o = new S2O();
         s2o.gui.setVisible(true);
+        
+        s2o.logger.log(Level.SEVERE, "SEVERE");
+        s2o.logger.log(Level.WARNING, "WARNING");
+        s2o.logger.log(Level.INFO, "INFO");
+        s2o.logger.log(Level.CONFIG, "CONFIG");
+        s2o.logger.log(Level.FINE, "FINE");
+        s2o.logger.log(Level.FINER, "FINER");
+        s2o.logger.log(Level.FINEST, "FINEST");
+        
+    }
+    
+    private void setupLogger()
+    {   
+        logger.setLevel(Level.ALL);
+        
+        textAreaHandler = new S2OTextAreaHandler(gui.getLogTextArea());
+        consoleHandler = new ConsoleHandler();
+        try {
+            fileHandler = new FileHandler("logs.txt");
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+        fileHandler.setFormatter(new SimpleFormatter());
+
+        logger.addHandler(textAreaHandler);
+        logger.addHandler(fileHandler);
         
     }
 
@@ -81,6 +118,16 @@ public class S2O {
     public S2OOscClient getOscClient() {
         return oscClient;
     }
+    
+    public S2OTextAreaHandler getTextAreaHandler() {
+        return textAreaHandler;
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+            
+    
     
     
     
